0acd6060fe7f311319cbacbcfde2f0d00f4b6458
Miguel Acuña <miguel.acuna.fabres@gmail.com>
2025-01-30 21:28:55 -0300

    Avance

--- index.js
+++ index.js
@@ -1,70 +1,59 @@
+const lat = -33.43107481500285;
+const long = -70.62097167231357;
 const baseUrl = 'https://appred.tstgo.cl/phone/v8/stopInfo?stopCode=STOPID&mode=3';
 const paraderos = 'https://raw.githubusercontent.com/imiguelacuna/donde-viene-la-micro/main/stops.json';
-
-const deg2rad = (deg) => deg * (Math.PI / 180);
-
-const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => 6371 * 2 * Math.atan2( Math.sqrt( Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2 ), Math.sqrt( 1 - (Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2) ) ) * 1000;
-
+const deg2rad = deg => deg * (Math.PI / 180);
+const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => 6371 * 2 * Math.atan2(Math.sqrt(Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2), Math.sqrt(1 - (Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2))) * 1000;
 const fetchData = async (url, headers = {}) => {
     let req = new Request(url);
     req.headers = headers;
     return await req.loadJSON();
-}
-
-const getServices = async (stopId) => {
+};
+const getServices = async stopId => {
+    var _response$predictions;
     const url = baseUrl.replace('STOPID', stopId);
-    const response = await fetchData(url, {"phone-id": "123456789"});
-    return response?.predictions?.map(x => ({
-        route: x?.route,
-        distanceLabel: x?.distanceLabel,
-        timeLabel: x?.timeLabel,
-        distance: x?.distance,
-        licensePlate: x?.licensePlate,
-    })) || [];
-}
-
-const sortStops = (stops) => stops.sort((a, b) => a.distance - b.distance);
-
-const createMenu = (stops) => stops.reduce((acc, curr)=> {
+    const response = await fetchData(url, {
+        "phone-id": "123456789"
+    });
+    return (response === null || response === void 0 || (_response$predictions = response.predictions) === null || _response$predictions === void 0 ? void 0 : _response$predictions.map(x => ({
+        route: x === null || x === void 0 ? void 0 : x.route,
+        distanceLabel: x === null || x === void 0 ? void 0 : x.distanceLabel,
+        timeLabel: x === null || x === void 0 ? void 0 : x.timeLabel,
+        distance: x === null || x === void 0 ? void 0 : x.distance,
+        licensePlate: x === null || x === void 0 ? void 0 : x.licensePlate
+    }))) || [];
+};
+const sortStops = stops => stops.sort((a, b) => a.distance - b.distance);
+const createMenu = stops => stops.reduce((acc, curr) => {
     acc[curr.stopId] = `${curr.distance} metros - ${curr.name}`;
     return acc;
 }, {});
-
-const createServices = (stops) => stops.reduce((acc, curr) => {
+const createServices = stops => stops.reduce((acc, curr) => {
     acc[curr.stopId] = curr.servicios.reduce((accServicio, currServicio) => {
         const info = `[${currServicio.licensePlate}] ${currServicio.timeLabel} (${currServicio.distanceLabel})`;
-        accServicio[currServicio.route] = accServicio[currServicio.route]
-            ? accServicio[currServicio.route] + ' - ' + info
-            : info;
+        accServicio[currServicio.route] = accServicio[currServicio.route] ? accServicio[currServicio.route] + ' - ' + info : info;
         return accServicio;
     }, {});
     return acc;
 }, {});
-
 const response = await fetchData(paraderos);
-
-let listadoParaderos = response
-    .map((item) => ({
-        stopId: item.stop_id,
-        name: item.name,
-        distance: Math.round(getDistanceFromLatLonInKm(lat, long, item.latitude, item.longitude)),
-        routes: item.routes,
-    }))
-    .filter((item) => item.distance <= 100);
-
+let listadoParaderos = response.map(item => ({
+    stopId: item.stop_id,
+    name: item.name,
+    distance: Math.round(getDistanceFromLatLonInKm(lat, long, item.latitude, item.longitude)),
+    routes: item.routes
+})).filter(item => item.distance <= 100);
 const servicesPromises = listadoParaderos.map(paradero => getServices(paradero.stopId));
 const allServices = await Promise.all(servicesPromises);
-
 listadoParaderos = listadoParaderos.map((paradero, index) => {
     paradero.servicios = allServices[index];
     return paradero;
 });
-
 listadoParaderos = sortStops(listadoParaderos);
-
 const menu = createMenu(listadoParaderos);
 const servicios = createServices(listadoParaderos);
-
-const output = { menu, servicios };
-
+const output = {
+    menu,
+    servicios
+};
 document.body.textContent = encodeURIComponent(JSON.stringify(output));
